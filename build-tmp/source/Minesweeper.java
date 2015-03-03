2a6e77b28c906bbfe6926daed67e1058d36d2e2a
unknown <segottlieb@697-334-Lorien.sf.k12.internal>
2015-03-03 10:27:25 -0800

    Created program, changed colors

--- build-tmp/source/Minesweeper.java
+++ build-tmp/source/Minesweeper.java
@@ -1,11 +1,29 @@
-import de.bezier.guido.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import de.bezier.guido.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Minesweeper extends PApplet {
+
+
 int NUM_ROWS = 20;
 int NUM_COLS = 20;
 private MSButton[][] buttons; //2d array of minesweeper buttons
 private ArrayList <MSButton> bombs; //ArrayList of just the minesweeper buttons that are mined
 private boolean gameOver = false;
 
-void setup ()
+public void setup ()
 {
     size(400, 400);
     textAlign(CENTER,CENTER);
@@ -144,15 +162,23 @@ public class MSButton
     public void draw () 
     {    
         if (marked)
+        {
+            buttons[r][c].setLabel("F");
             fill(81, 15, 173);
+        }
         else if(clicked && bombs.contains(this)) 
+        {
             fill(255, 205, 0 );
+        }
         else if(clicked)
+        {
             fill(5, 125, 159);
+        }
         else 
+        {
             fill(23, 41, 176);
+        }
 
-        
         rect(x, y, width, height);
         fill(81, 15, 173);
         text(label,x+width/2,y+height/2);
@@ -186,4 +212,13 @@ public class MSButton
             numBombs++;           
         return numBombs;
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
